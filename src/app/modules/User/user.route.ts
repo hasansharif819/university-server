acba61e9a8dd2c03c13cad223774799fa6723a43
hasansharif819 <hs.sharif819@gmail.com>
2024-02-09 00:28:39 +0600

    file is comment out from creating admin, student and faculty

--- src/app/modules/User/user.route.ts
+++ src/app/modules/User/user.route.ts
@@ -1,8 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import express, { NextFunction, Request, Response } from 'express';
+import express from 'express';
+// import express, { NextFunction, Request, Response } from 'express';
 import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
-import { upload } from '../../utils/sendImageToCloudinary';
+// import { upload } from '../../utils/sendImageToCloudinary';
 import { createAdminValidationSchema } from '../Admin/admin.validation';
 import { createFacultyValidationSchema } from '../Faculty/faculty.validation';
 import { createStudentValidationSchema } from '../Student/student.validation';
@@ -15,11 +16,11 @@ const router = express.Router();
 router.post(
   '/create-student',
   auth(USER_ROLE.superAdmin, USER_ROLE.admin),
-  upload.single('file'),
-  (req: Request, res: Response, next: NextFunction) => {
-    req.body = JSON.parse(req.body.data);
-    next();
-  },
+  // upload.single('file'),
+  // (req: Request, res: Response, next: NextFunction) => {
+  //   req.body = JSON.parse(req.body.data);
+  //   next();
+  // },
   validateRequest(createStudentValidationSchema),
   UserControllers.createStudent,
 );
@@ -27,11 +28,11 @@ router.post(
 router.post(
   '/create-faculty',
   auth(USER_ROLE.superAdmin, USER_ROLE.admin),
-  upload.single('file'),
-  (req: Request, res: Response, next: NextFunction) => {
-    req.body = JSON.parse(req.body.data);
-    next();
-  },
+  // upload.single('file'),
+  // (req: Request, res: Response, next: NextFunction) => {
+  //   req.body = JSON.parse(req.body.data);
+  //   next();
+  // },
   validateRequest(createFacultyValidationSchema),
   UserControllers.createFaculty,
 );
@@ -39,11 +40,11 @@ router.post(
 router.post(
   '/create-admin',
   auth(USER_ROLE.superAdmin, USER_ROLE.admin),
-  upload.single('file'),
-  (req: Request, res: Response, next: NextFunction) => {
-    req.body = JSON.parse(req.body.data);
-    next();
-  },
+  // upload.single('file'),
+  // (req: Request, res: Response, next: NextFunction) => {
+  //   req.body = JSON.parse(req.body.data);
+  //   next();
+  // },
   validateRequest(createAdminValidationSchema),
   UserControllers.createAdmin,
 );
